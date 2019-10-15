e542b09faed792a397978eae7c37e5a7083b396f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-15 16:06:45 -0400

    [Add] Completed Array Challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -71,12 +71,18 @@ let firstNamesAllCaps = [];
 runners.map(runner => {
   firstNamesAllCaps.push(runner.first_name.toUpperCase());
 })
-console.log(firstNamesAllCaps);
+console.log(firstNamesAllCaps, 'first name all caps');
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
-console.log(runnersLargeSizeShirt);
+// let runnersLargeSizeShirt = [];
+
+const runnersLargeSizeShirt = runners.filter(runner => runner.shirt_size === 'L')
+
+
+console.log(runnersLargeSizeShirt, 'large shirt');
+
+
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
